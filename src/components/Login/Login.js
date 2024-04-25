35ce7579be921d19d037a29dd88c884147d6084b
QUOC NHU <nquocnhu95@gmail.com>
2024-04-25 21:15:00 +0700

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,35 +1,44 @@
-import './Login.scss'
+import "./Login.scss";
 
 const Login = (props) => {
-    return (
-        <div className="login-container mt-5">
-            <div className="container">
-                <div className="row">
-                    {/* d-none d-sm-block block left on small devices*/}
-                    <div className="content-left col-7 d-none d-sm-block">
-                        <div className="brand">
-                            QUOC NHU'S FULLSTACK
-                        </div>
-                        <div className="detail">
-                            No pain - No gain
-                        </div>
-                    </div>
-                    {/* big device 12 items, sm 5items */}
-                    <div className="content-right col-12 col-sm-5 red d-flex flex-column py-3 gap-1">
-                        <input type="text" className="form-control" placeholder="Enter your email"/>
-                        <input type="password" className="form-control" placeholder="Enter your password"/>
-                        <button className="btn btn-primary">Login</button>
-                        <span className="text-center">Forgot your password?</span>
-                        {/* hr to make a line */}
-                        <hr />
-                        <button className="btn btn-success">Create new account</button>
-                    </div>
-                </div>
-
-            </div>
-
+  return (
+    <div className="login-container">
+      <div className="container">
+        {/* sometimes we just edit css for father */}
+        <div className="row px-3 px-sm-0">
+          {/* d-none d-sm-block block left on small devices*/}
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">QUOC NHU'S FULLSTACK</div>
+            <div className="detail">No pain - No gain</div>
+          </div>
+          {/* big device 12 items, sm 5items */}
+          <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+            {/* show logo when less than sm */}
+            <div className="brand d-sm-none">QUOC NHU'S FULLSTACK</div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your email"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter your password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">
+              <a href="#" className="forgot-password">
+                Forgot your password?
+              </a>
+            </span>
+            {/* hr to make a line */}
+            <hr />
+            <button className="btn btn-success">Create new account</button>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
